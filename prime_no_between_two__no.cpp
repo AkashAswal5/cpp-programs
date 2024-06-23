59bf9a1cea64e20f443df9b0307fa511c896ec27
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2024-06-23 17:53:14 +0530

    Update prime_no_between_two__no.cpp

--- prime_no_between_two__no.cpp
+++ prime_no_between_two__no.cpp
@@ -1,10 +1,39 @@
 #include<iostream>
 using namespace std;
 
+bool isprime(int num){
+    if(num<=1)return false;
+    for(int i=2 ; i*i<=num ; ++i){
+        if(num%i==0){return false ;}
+    }
+    return true;
+
+}
+
+
+void printprime(int a, int b){
+    for(int num=a ; num<=b ; ++num){
+        if(isprime(num)){
+            cout<<num<<" ";
+        }
+    }
+    cout<<endl;
+
+
+}
+
 
 int main(){
 
-  
+// print prime number between two prime number
+
+int x,y;
+cin>>x>>y;
+
+printprime(x,y);
+
+
+
 
 return 0 ;
 }
