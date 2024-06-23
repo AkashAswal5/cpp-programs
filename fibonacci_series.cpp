3fda39327dc1fa2b541eed8d4494bd2363e0eafa
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2024-06-23 19:04:15 +0530

    Create fibonacci_series.cpp

---
+++ fibonacci_series.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+
+int sum=0;
+    int alpha =1;
+int a=0;
+
+for(int i=0 ; i<=n ; i++){
+    
+    cout<< sum << " ";     // sum=0 alpha =1
+    a=alpha;
+alpha =sum; // alpha =0
+sum=a; //sum=1
+
+    sum=sum+alpha; // sum=1 
+
+       
+
+    //sum=1 alpha=1
+
+
+    
+
+  
+
+
+   
+
+
+}
+
+}
+
+int main(){
+
+// fibenacci Sequence
+// 0 , 1 ,1, 2, 3 ,5 , 8 , 13  , 21 , .. ... . .. . 
+
+int num;
+
+cin>>num;
+fibonacci(num);
+
+return 0;
+}
