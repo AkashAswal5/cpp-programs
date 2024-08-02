d79d730d99260392df3d4e26ba1abc79f59e6df1
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2024-08-02 20:25:50 +0530

    Create binary search .cpp

---
+++ binary search .cpp
@@ -0,0 +1,47 @@
+//binary search 
+
+
+#include <iostream>
+using namespace std;
+
+int binarysearch(int n , int arr[] , int key){
+    int s=0 , e=n ;
+while (s<=e){
+    int mid=(s+e)/2;
+
+if(arr[mid]==key){
+    return mid;
+}
+
+else if (arr[mid]>key){
+    e=mid-1;
+}
+else {
+    s=mid+1;
+}
+}
+return -1;
+
+}
+
+int main(){
+
+int n;
+cout<<"enter n " <<endl;
+cin>>n;
+
+cout << "enter number in array :  " <<"endl ";
+int arr[n];
+
+for (int i=0 ; i<n ; i++){
+    cin>>arr[i];
+}
+
+int key;
+cout<<"enter key " ;
+cin>> key;
+
+cout<<binarysearch(n , arr , key);
+
+    return 0;
+}
