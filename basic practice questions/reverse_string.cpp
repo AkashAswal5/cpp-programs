e9bcc0e640b06cebe0a0bc053647ab5bfcab4950
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2024-12-16 10:33:42 +0530

    Create reverse_string.cpp

---
+++ basic practice questions/reverse_string.cpp
@@ -0,0 +1,22 @@
+// reverse a string
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+string str1;
+cout<<"enter String: ";
+cin>>str1;
+
+int n=str1.length();
+
+for(int i=0 ; i<n/2 ; i++){
+    swap(str1[i],str1[n-i-1]);
+}
+
+cout<<" reverse string: "<<str1;
+
+
+    return 0;
+}
