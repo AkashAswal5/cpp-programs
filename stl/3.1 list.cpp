6c8ec177e5360daa6d7271a11596b1bf207b77c1
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-08 20:32:02 +0530

    Create 3.1 list.cpp

---
+++ stl/3.1 list.cpp
@@ -0,0 +1,16 @@
+/* 
+list is a container in the STL,
+ that implements a doubly linked list.
+
+  Unlike vector, which provides a dynamic array, 
+  
+  a list allows efficient insertion and deletion of elements at
+   both ends or in the middle of the container.
+
+
+
+   Key Features of list:
+Doubly Linked List: Each element is linked to both its previous and next elements.
+Efficient Insert/Delete: Fast insertion and deletion at any position (O(1) time for most operations).
+No Random Access: Elements cannot be accessed directly using indices like in vector.
+*/
