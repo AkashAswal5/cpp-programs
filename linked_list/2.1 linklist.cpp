139618f22201773d8e8a9f450c66dad44ed9322c
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-16 22:20:30 +0530

    Update 2.1 linklist.cpp

--- linked_list/2.1 linklist.cpp
+++ linked_list/2.1 linklist.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+// add , insert
+// print linklist
+// delete node
+// size check
+
+// linked list node
+struct node
+{
+    int data;
+    node *next;
+};
+
+void printlist(node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->data << endl;
+        n = n->next;
+    }
+}
+
+int main()
+{
+
+    // create node and address at head, second, third
+    node *head = new node();
+    node *second = new node();
+    node *third = new node();
+
+    head->data = 5;
+    head->next = second;
+
+    second->data = 1;
+    second->next = third;
+
+    third->data = 5;
+    third->next = NULL;
+
+    printlist(head);
+
+    return 0;
+}
