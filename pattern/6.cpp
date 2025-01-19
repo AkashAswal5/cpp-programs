2b80b83c8d518897d056d0750f185806bdcf9609
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-19 21:36:10 +0530

    Update 6.cpp

--- pattern/6.cpp
+++ pattern/6.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 int main()
@@ -23,5 +24,28 @@ int main()
         cout << endl;
     }
 
+    /*
+        *
+       ***
+      *****
+     *******
+    *********
+
+    */
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
     return 0;
 }
