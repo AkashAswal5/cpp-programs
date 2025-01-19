c188831c9d20da170ba56ef9d1340bb8d08bf3d2
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-19 21:48:07 +0530

    Create 6.cpp

---
+++ pattern/6.cpp
@@ -0,0 +1,53 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+
+    /*
+
+        *
+       ***
+      *****
+     *******
+    *********
+     *******
+      *****
+       ***
+        *
+
+    */
+
+    // upper half
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // lower half
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+
+        for (int j = n; j > i; j--)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
