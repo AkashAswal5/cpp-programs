a2f04288f5da202e7df707ed77dcb4191aaaaa95
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-26 22:03:10 +0530

    Update 1.cpp

--- DSA-questions/1.cpp
+++ DSA-questions/1.cpp
@@ -1,1 +1,41 @@
+/*
+1.	 Find the number of digits in a number
+Description: Find how many digits are in a given number.
+Example:
+Input: 12345
+Output: 5
 
+*/
+
+#include <iostream>
+using namespace std;
+
+int countdigit(int n)
+{
+    int digit = 0, rem = 0;
+    int count = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num == 0)
+    {
+        cout << "Number of digit: 1 " << endl;
+    }
+    else
+    {
+        cout << " number of digit: " << countdigit(num) << endl;
+    }
+
+    return 0;
+}
