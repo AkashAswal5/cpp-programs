de3016c646cad112b2369030927863b1e7822bc2
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-02-09 11:37:09 +0530

    Update 12.cpp

--- DSA-questions/12.cpp
+++ DSA-questions/12.cpp
@@ -1,49 +1,58 @@
 /*
-12.	Check if a number is a factor of another number 
-Description: Check if one number is a prime factor of another number. 
-Example: 
-Input: 10, 20 
-Output: True (10 is a factor of 20) 
+12.	Check if a number is a factor of another number
+Description: Check if one number is a prime factor of another number.
+Example:
+Input: 10, 20
+Output: True (10 is a factor of 20)
 */
 
 #include <iostream>
 using namespace std;
 
 // Function to check if a number is prime
-bool isPrime(int num) {
-    if (num <= 1) return false;
-    for (int i = 2; i * i <= num; i++) {
+bool isPrime(int num)
+{
+    if (num < 2)
+        return false;
+    for (int i = 2; i * i <= num; i++)
+    {
         if (num % i == 0)
             return false;
     }
     return true;
 }
 
-int main() {
-    int n1, n2;
+int main()
+{
+    int num1, num2;
 
-    cout << "Program to check if a number is a factor of another number.\n";
     cout << "Enter num1: ";
-    cin >> n1;
+    cin >> num1;
     cout << "Enter num2: ";
-    cin >> n2;
+    cin >> num2;
 
-    if (n1 > n2) {
-        cout << "Error: num1 should be smaller than or equal to num2.\n";
-        return 1;
+    // Validate input (positive numbers only)
+    if (num1 <= 0 || num2 <= 0)
+    {
+        cout << "Both numbers must be positive and greater than zero." << endl;
+        return 0;
     }
 
-    if (n2 % n1 == 0) {
-        cout << "True, " << n1 << " is a factor of " << n2 << ".\n";
-        
-        // Check if the factor is prime
-        if (isPrime(n1)) {
-            cout << n1 << " is also a prime factor of " << n2 << ".\n";
-        } else {
-            cout << n1 << " is not a prime factor of " << n2 << ".\n";
+    // Check if num1 is a factor of num2
+    if (num2 % num1 == 0)
+    {
+        if (isPrime(num1))
+        {
+            cout << num1 << " is a prime factor of " << num2 << endl;
         }
-    } else {
-        cout << "False, " << n1 << " is not a factor of " << n2 << ".\n";
+        else
+        {
+            cout << num1 << " is a factor of " << num2 << " but not a prime factor." << endl;
+        }
+    }
+    else
+    {
+        cout << num1 << " is not a factor of " << num2 << endl;
     }
 
     return 0;
