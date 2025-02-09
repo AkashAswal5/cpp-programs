a896c3798a8a7972d656d57f03e77b8167ef55e3
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-02-09 11:11:25 +0530

    Update 12.cpp

--- DSA-questions/12.cpp
+++ DSA-questions/12.cpp
@@ -6,24 +6,45 @@ Input: 10, 20
 Output: True (10 is a factor of 20) 
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-int n1 , n2;
-cout<<"Program to check if a number is a factor of another number";
-cout<<"Enter num1: ";
-cin>>n1;
-cout<<"enter num2: ";
-cin>>n2;
 
-if ( n1<n2) {
-if(n2%n1==0){
-    cout<<"True "<<n1<<" is Factor of "<<n2;
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
 }
-};
-else{
-    cout<<n1<< " is not factor of "<<n2;
-}
+
+int main() {
+    int n1, n2;
+
+    cout << "Program to check if a number is a factor of another number.\n";
+    cout << "Enter num1: ";
+    cin >> n1;
+    cout << "Enter num2: ";
+    cin >> n2;
+
+    if (n1 > n2) {
+        cout << "Error: num1 should be smaller than or equal to num2.\n";
+        return 1;
+    }
+
+    if (n2 % n1 == 0) {
+        cout << "True, " << n1 << " is a factor of " << n2 << ".\n";
+        
+        // Check if the factor is prime
+        if (isPrime(n1)) {
+            cout << n1 << " is also a prime factor of " << n2 << ".\n";
+        } else {
+            cout << n1 << " is not a prime factor of " << n2 << ".\n";
+        }
+    } else {
+        cout << "False, " << n1 << " is not a factor of " << n2 << ".\n";
+    }
 
     return 0;
 }
