42916324f377fb95f002f5edfcf56d52465e823e
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-02-10 21:50:17 +0530

    Create 19.cpp

---
+++ DSA-questions/19.cpp
@@ -0,0 +1,21 @@
+/*
+19.	Find the Number of Unique Elements in an Array:   25
+Description: Count the distinct elements in an array. 
+Input: [1, 2, 2, 3, 4, 4, 5] 
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+     cout<<"Enter how many number to Enter: ";
+
+    int arr[n];
+    cout<<"Enter number of element in an array: ";
+    for(int i=0; i<n ; i++){
+        cin>>arr[i];
+    }
+
+    
+    return 0;
+}
