a540f5c93fa90636102a3079eb672fd1dbf939e7
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-02-10 21:33:09 +0530

    Update 18.cpp

--- DSA-questions/18.cpp
+++ DSA-questions/18.cpp
@@ -1,1 +1,26 @@
+/*18.
+Reverse a string  15
+Description: Write a program to reverse the characters of a given string.
+Example:
+Input: "hello"
+Output: "olleh"
+*/
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+
+    char str1[100];
+    cout << "Enter a String: ";
+    cin.getline(str1, 100);
+
+    int len = strlen(str1);
+
+    for (int i = len; i >= 0; i--)
+    {
+        cout << str1[i];
+    }
+    return 0;
+}
