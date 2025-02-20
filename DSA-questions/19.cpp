602cfecda682b4302193e96043dd21e4a05f1ecc
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-02-20 20:48:17 +0530

    Update 19.cpp

--- DSA-questions/19.cpp
+++ DSA-questions/19.cpp
@@ -1,35 +1,39 @@
 /*
-19.	Find the Number of Unique Elements in an Array:   
-Description: Count the distinct elements in an array. 
-Input: [1, 2, 2, 3, 4, 4, 5] 
+19.	Find the Number of Unique Elements in an Array:
+Description: Count the distinct elements in an array.
+Input: [1, 2, 2, 3, 4, 4, 5]
+output: 5
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
+#include <set> // Include set Library
 
-int uniqueelement(int arr[],int n){
-    int count=0;
-    for(int i=0 ; i<n; i++){
-        for(int j=i+1; j<n ; j++){
-            if(arr[i]==arr[j]){
-                count++;
-            }
-        }
-    }
-    cout<<count;
+int uniqueelement(int arr[], int n)
+{
+  set<int> uniqueelements; // set automatically stores unique values
+  for (int i = 0; i < n; i++)
+  {
+    uniqueelements.insert(arr[i]);
+  }
+
+  return uniqueelements.size(); // return the count of unique elements
 }
-int main(){
-     int n;
-     cout<<"Enter how many number to Enter: ";
-     cin>> n;
 
-    int arr[n];
-    cout<<"Enter number of element in an array: ";
-    for(int i=0; i<n ; i++){
-        cin>>arr[i];
-    }
+int main()
+{
+  int n;
+  cout << "Enter how many number to Enter: ";
+  cin >> n;
 
-    uniqueelement(arr,n);
-    
-    return 0;
+  int arr[n];
+  cout << "Enter number of element in an array: ";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  cout << "Numnber of Unique elements: " << uniqueelement(arr, n) << endl;
+
+  return 0;
 }
